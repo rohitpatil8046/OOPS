e255bf5d159c4e93f28830d96dd5407b39d7dd46
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-02 16:32:57 +0530

    Add files via upload

---
+++ Abstraction1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rohitconsoleapp
+{
+    internal class Abstraction1
+    {
+        public class Laptop
+        {
+            private string brand;
+            private string model;
+            public string Brand
+            {
+                get { return brand; }
+                set { brand = value; }
+            }
+            public string Model
+            {
+                get { return model; }
+                set { model = value; }
+            }
+            public void LaptopDetails()
+            {
+                Console.WriteLine("Brand: " + Brand);
+                Console.WriteLine("Model: " + Model);
+            }
+            public void LaptopKeyboard()
+            {
+                Console.WriteLine("Type using Keyword");
+            }
+            private void MotherBoardInfo()
+            {
+                Console.WriteLine("MotheBoard Information");
+            }
+            private void InternalProcessor()
+            {
+                Console.WriteLine("Processor Information");
+            }
+        }
+    }
+}
