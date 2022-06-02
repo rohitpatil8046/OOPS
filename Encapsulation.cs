7dbff24597e7f62cf720b455609365200f4121fb
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-02 17:02:02 +0530

    Add files via upload

---
+++ Encapsulation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rohitconsoleapp
+{
+    internal class Encapsulation
+    {
+        public class Example
+        {
+            public int x;
+        }
+        class Sample
+        {
+            public static void Main()
+            {
+                Example e = new Example();
+                e.x = 50;
+                Console.WriteLine(e.x);
+                e.x = -10;
+                Console.WriteLine(e.x);
+                Console.WriteLine("Press any key to exist");
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
