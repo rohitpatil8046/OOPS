5cb4c368a06efe87b7bdad40668c4b85d8334e4c
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-02 16:28:01 +0530

    Add files via upload

---
+++ ClassObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rohitconsoleapp
+{ 
+   
+     class Program
+    {
+        string color = "White";
+
+
+
+
+
+        static void Main(string[] args)
+        {
+          Program myObj1 = new Program();
+           
+            Console.WriteLine(myObj1.color);
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
