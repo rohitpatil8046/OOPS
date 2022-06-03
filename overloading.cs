89effe6437bfa4b122e05235914b7f1474300701
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-03 16:30:29 +0530

    Add files via upload

---
+++ overloading.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    internal class overloading
+    {
+        void Sum(int a ,int b)
+        {
+          Console.WriteLine(a+b);
+        }
+        void Sum(float a ,float b)
+        {
+          Console.WriteLine(a+b);
+        }
+        void Sum(float a, float b,float c)
+        {
+            Console.WriteLine(a + b+c);
+        }
+        public static void Main(string[] args)
+            {
+                overloading obj = new overloading();
+                obj.Sum(10, 20,30);
+                Console.ReadKey();
+            }
+        }
+    }
+
+    
+
+
+    
