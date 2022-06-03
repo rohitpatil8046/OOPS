5b3611e272ffb72db29845a470aeae26b7d808d2
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-03 18:26:01 +0530

    Add files via upload

---
+++ Interface.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    internal class Polygon
+    {
+        interface IPolygon
+        {
+            // method without body
+            void calculateArea(int l, int b);
+
+        }
+
+        class Rectangle : IPolygon
+        {
+
+            // implementation of methods inside interface
+            public void calculateArea(int l, int b)
+            {
+
+                int area = l * b;
+                Console.WriteLine("Area of Rectangle: " + area);
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Rectangle r1 = new Rectangle();
+
+                r1.calculateArea(100, 200);
+                Console.ReadLine();
+
+            }
+
+         
+        }
+       
+    }
+}
+
+
+
+
+
