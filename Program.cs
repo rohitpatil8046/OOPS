526a2313922838197f7daaa12aeedc5f5a793c03
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-03 16:12:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typecasting
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            //Type Casting Program
+            int x = (int)3.55;
+            float y = (float)6.887;
+            double z = 'a';
+            long w = 87876549866565636;
+            float f = Convert.ToInt32(3.44);
+            string r = Convert.ToString("Rohit is a Software Developer");
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(w);
+            Console.WriteLine(f);
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+    }
+}
