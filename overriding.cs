3a77cb9cbc7c45265036fab9dd14e803ceed67fd
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-03 17:30:56 +0530

    Add files via upload

---
+++ overriding.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    internal class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating");
+        }
+    }
+    internal class Dog : Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Dog is eating");
+        }
+
+        public static void Main(string[] args)
+        {
+            Animal puppy = new Dog();
+            puppy.Eat();
+            Console.ReadLine();
+        }
+    }
+}
+
+
+    
